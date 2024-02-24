37fc90b0661ed6bd539bdf8bf171fc823c15730f
tyler23456 <89640257+tyler23456@users.noreply.github.com>
2024-02-24 02:23:39 -0500

    -created elevator interactivity template

---
+++ TetheredGame/Assets/_Scripts/Activator/CallElevatorActivator.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TG.Activator
+{
+    public class CallElevatorActivator : ActivatorBase, IActivator
+    {
+
+
+        public override void Activate()
+        {
+            
+        }
+    }
+}
---
+++ TetheredGame/Assets/_Scripts/Activator/ElevatorActivator.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TG.Activator
+{
+    public class ElevatorActivator : ActivatorBase, IActivator
+    {
+        [SerializeField] CallElevatorActivator callElevatorActivator;
+
+        public override void Activate()
+        {
+            
+        }
+    }
+}
